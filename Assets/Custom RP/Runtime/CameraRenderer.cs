d880f241dc011cf067acb924ab48ae2f0ccbea38
PotroChen <1400539511@qq.com>
2021-01-25 02:02:15 +0800

    提交

---
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    const string bufferName = "Render Camera";
+    static ShaderTagId 
+        unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit"),
+        litShaderTagId = new ShaderTagId("CustomLit");
+
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    CommandBuffer buffer = new CommandBuffer { name = bufferName };
+
+    CullingResults cullingResults;
+
+    Lighting lighting = new Lighting();
+
+    public void Render(ScriptableRenderContext context, Camera camera,
+        bool useDynamicBatching, bool useGPUInstancing,
+        ShadowSettings shadowSettings)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        PrepareBuffer();
+        PrepareForSceneWindow();
+        if (!Cull(shadowSettings.maxDistance))
+        {
+            return;
+        }
+
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+        lighting.Setup(context, cullingResults, shadowSettings);
+        buffer.EndSample(SampleName);
+
+        Setup();
+        DrawVisibleGeometry(useDynamicBatching, useGPUInstancing);
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
+        buffer.ClearRenderTarget(//SetupCameraProperties在ClearRenderTarget之前更有效率，但还是不知道为啥
+            flags <= CameraClearFlags.Depth,
+            flags == CameraClearFlags.Color,
+            flags == CameraClearFlags.Color ? camera.backgroundColor.linear : Color.clear
+            );
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+    }
+
+    void DrawVisibleGeometry(bool useDynamicBatching, bool useGPUInstancing)
+    {
+        var sortingSettings = new SortingSettings(camera) { 
+            criteria = SortingCriteria.CommonOpaque //渲染顺序
+        };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings) 
+        {
+            enableDynamicBatching = useDynamicBatching,
+            enableInstancing = useGPUInstancing
+        };
+        drawingSettings.SetShaderPassName(1, litShaderTagId);
+        //1.先渲染不透明物体
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+        context.DrawRenderers(
+            cullingResults, ref drawingSettings, ref filteringSettings
+        );
+        //2.渲染天空盒
+        context.DrawSkybox(camera);
+
+        //3.渲染透明物体
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+
+        context.DrawRenderers(
+            cullingResults, ref drawingSettings, ref filteringSettings
+        );
+    }
+
+    void Submit()
+    {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        lighting.Cleanup();
+        context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    bool Cull(float maxShadowDistance)
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            p.shadowDistance = Mathf.Min(maxShadowDistance, camera.farClipPlane);
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+}
